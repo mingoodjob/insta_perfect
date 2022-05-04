e5eccd7fed8f47e79bc58e110a8d983c4f7649af
KIM HAJIN <rlagkwls629@gmail.com>
2022-05-04 15:22:29 +0900

    login 화면 레이아웃 디자인 완성 iphone 박스슬라이더를 이용한 자동슬라이더 기능 구현

---
+++ static/image_slide_js.js
@@ -0,0 +1,13 @@
+// 박스 슬라이더 js
+$(document).ready(function() {
+    $('.bxslider').bxSlider({
+        controls: false,
+        speed: 2000,
+        auto: true,
+        pager: false,
+        mode: 'fade',
+        pause: 3000,
+    });
+});
+
+
