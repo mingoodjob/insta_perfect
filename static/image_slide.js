b8cf63c9d1902825298ea8bf899f352ebcc22eed
KIM HAJIN <rlagkwls629@gmail.com>
2022-05-04 21:17:48 +0900

    login 화면 레이아웃 디자인 완성 iphone 박스슬라이더를 이용한 자동슬라이더 기능 구현 login ID칸에 텍스트 입력시 span태그 올라가고 text 입력 login pw칸에 텍스트 입력시 span태그 올라가고 pw 입력 pw 입력시 비밀번호 표시 추가 누르면 숨기기 기능 구현 id,pw 입력시 버튼 활성화 (조건 pw6글자 이상)

--- static/image_slide.js
+++ static/image_slide.js
@@ -1,3 +1,4 @@
+'use strict';
 $(document).ready(function() {
 
         $('.bxslider').bxSlider({
@@ -23,23 +24,40 @@ login_input.addEventListener('keyup', (event) => {
 const login_input2 = document.querySelector(".login_input2");
 const login_input_text2 = document.querySelector(".login_input_text2");
 const login_hide_pwd = document.querySelector(".login_hide_pwd");
+const login_btn = document.querySelector(".login_btn");
 // var id_value = document.getElementById('id_value').value;
 // var id_value = document.getElementById('pwd_value').value;
 
-login_input2.addEventListener('keyup', (event) => {
+login_input2.addEventListener('keydown', (event) => {
     login_input_text2.style.fontSize = "5px";
     login_input_text2.style.lineHeight = "1px";
     login_hide_pwd.style.display = "flex";
      login_input2.style.padding ="7px 0 1px 4px";
-     })
+
      //비밀번호 숨기기 기능
      login_hide_pwd.addEventListener('click', (event) => {
         $('.login_input2').prop("type","text");
 
+    login_hide_pwd.addEventListener('click', (event) => {
+        $('.login_input2').prop("type", "password");
+    })
+    })
 
+     let idValue = login_input.value;
+     let pwValue = login_input2.value;
+
+     if(
+         (idValue && pwValue) &&
+         (pwValue.length >= 5) &&
+         (idValue.length >= 1)
+     ) {
+         login_btn.style.opacity = 1;
+     }
+     else {
+         login_btn.style.opacity = .3;
+     }
 
 });
-
 
 
 //
