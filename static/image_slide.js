53f172b338e5759181eb09871759cd7dffc966af
KIM HAJIN <rlagkwls629@gmail.com>
2022-05-04 20:08:48 +0900

    login 화면 레이아웃 디자인 완성 iphone 박스슬라이더를 이용한 자동슬라이더 기능 구현 login ID칸에 텍스트 입력시 span태그 올라가고 text 입력 login pw칸에 텍스트 입력시 span태그 올라가고 pw 입력 pw 입력시 비밀번호 표시 추가 누르면 숨기기 기능 구현

---
+++ static/image_slide.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+        $('.bxslider').bxSlider({
+            controls: false,
+            speed: 2000,
+            auto: true,
+            pager: false,
+            mode: 'fade',
+            pause: 3000,
+        });
+
+ });
+
+// 로그인 input 입력시 글자이동 //
+const login_input = document.querySelector(".login_input");
+const login_input_text = document.querySelector(".login_input_text");
+login_input.addEventListener('keyup', (event) => {
+    login_input_text.style.fontSize = "5px";
+    login_input_text.style.lineHeight = "1px";
+     login_input.style.padding ="7px 0 1px 4px";
+})
+
+const login_input2 = document.querySelector(".login_input2");
+const login_input_text2 = document.querySelector(".login_input_text2");
+const login_hide_pwd = document.querySelector(".login_hide_pwd");
+// var id_value = document.getElementById('id_value').value;
+// var id_value = document.getElementById('pwd_value').value;
+
+login_input2.addEventListener('keyup', (event) => {
+    login_input_text2.style.fontSize = "5px";
+    login_input_text2.style.lineHeight = "1px";
+    login_hide_pwd.style.display = "flex";
+     login_input2.style.padding ="7px 0 1px 4px";
+     })
+     //비밀번호 숨기기 기능
+     login_hide_pwd.addEventListener('click', (event) => {
+        $('.login_input2').prop("type","text");
+
+
+
+});
+
+
+
+//
+
