43edaeb9d1bc9aa0bb0f0b7db1610b46ed769bf8
이민기 <psjlmk@gmail.com>
2022-05-06 14:55:11 +0900

    fix - app.py 경로 수정 , html 일부 파일 수정

--- app.py
+++ app.py
@@ -3,32 +3,12 @@ import os, hashlib
 
 app = Flask(__name__)
 
-
-@app.route("/join", methods=["POST"])
-def join_post():
-    uid_receive = request.form['uid_give']
-    name_receive = request.form['name_give']
-    pwd_receive = request.form['pwd_give']
-    hashed_pw = hashlib.sha256(pwd_receive.encode('utf-8')).hexdigest()
-    pr_photo_receive = request.form['pr_photo_give']
-
-    doc = {
-        'uid': uid_receive,
-        'name': name_receive,
-        'pwd': hashed_pw,
-        'pr_photo': pr_photo_receive
-    }
-
-    db.joinusers.insert_one(doc)
-
-    return jsonify({'response':'success', 'msg':'환영합니다!'})
-
 @app.route('/')
 def home():
   return render_template('feed.html')
 
 @app.route('/login')
-def feed():
+def login():
   return render_template('login.html')
 
 @app.route('/login_check', methods =['POST'])
@@ -46,6 +26,7 @@ def join_page():
     return render_template('join.html')
 
 @app.route("/join", methods=["POST"])
+
 def join_post():
     uid_receive = request.form['uid_give']
     name_receive = request.form['name_give']
