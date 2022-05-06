59228d874391436bcced7ee029b3378a8b8d0af9
seonyoungBaek <rk4411@naver.com>
2022-05-06 13:20:40 +0900

    회원가입페이지 app.py

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-# 파일 저장
-@app.route('/api', methods=['POST'])
-def get_file():
-    image = request.file['image']
-    print(f'{image}')
-#   image.save(f'./{image.filename}')
-    return render_template('profile.html')
+import hashlib
 
-# 시작 페이지 로드
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://please_remember:camp14@cluster0.qstcy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client.db14
+
+
+
+
+
 @app.route('/')
-def Go_start():
-    return render_template('feed.html')  # 작업 편의성을 위해 임시로 피드페이지로 바로 로드
+def home():
+    return render_template('join.html')
 
-# 피드 페이지 로드
-@app.route('/feed')
-def Go_feed():
-    return render_template('feed.html')
+@app.route("/join", methods=["POST"])
+def join_post():
+    uid_receive = request.form['uid_give']
+    name_receive = request.form['name_give']
+    pwd_receive = request.form['pwd_give']
+    hashed_pw = hashlib.sha256(pwd_receive.encode('utf-8')).hexdigest()
+    pr_photo_receive = request.form['pr_photo_give']
 
-# 프로필 페이지 로드
-@app.route('/profile')
-def Go_profile():
-    return render_template('profile.html')
+    doc = {
+        'uid': uid_receive,
+        'name': name_receive,
+        'pwd': hashed_pw,
+        'pr_photo': pr_photo_receive
+    }
+
+    db.joinusers.insert_one(doc)
+
+    return jsonify({'response':'success', 'msg':'환영합니다!'})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
